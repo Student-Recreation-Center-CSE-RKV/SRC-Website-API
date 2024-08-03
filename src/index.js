c009e1b36a4eeba7b0d6a842e44fda6d3f955362
rgukt-rkv-cse <pc_coordinator@rguktrkv.ac.in>
2024-08-03 21:44:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ const app = express();
 const { PORT } = require("./config/serverConfig");
 var bodyParser = require("body-parser");
 const cors = require("cors"); // Import the CORS middleware
-app.use(cors()); // Use the CORS middleware
-
+// app.use(cors()); // Use the CORS middleware
+app.use(cors({
+    origin: 'https://rgukt-rkv-cse-src.vercel.app', 
+    // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+    // allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public/images', express.static('public/images'));
